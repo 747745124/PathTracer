b00e9b9a0463fdc0d3139145018bff80ca0a1607
Naoyuki <zhaonzh@gmail.com>
2025-05-08 03:40:49 -0700

    problem fixed, change epsilon to address the issue

--- include/primitives/bvh.hpp
+++ include/primitives/bvh.hpp
@@ -5,12 +5,13 @@
 #include <algorithm>
 #include <cstdlib>
 
-class BVHNode : public Hittable {
+class BVHNode : public Hittable
+{
 public:
   BVHNode() = default;
   BVHNode(ObjectList &object_list, uint i, uint j, float t0, float t1);
   BVHNode(ObjectList &object_list, float t0 = 0.0f, float t1 = 1.0f)
-      : BVHNode(object_list, 0, object_list.getLists().size(), t0, t1){};
+      : BVHNode(object_list, 0, object_list.getLists().size(), t0, t1) {};
 
   bool intersect(const Ray &ray, HitRecord &hit_record, float tmin,
                  float tmax) const override;
@@ -24,9 +25,11 @@ public:
 };
 
 inline bool BVHNode::intersect(const Ray &ray, HitRecord &hit_record,
-                               float tmin = 0.0001, float tmax = 1e5) const {
+                               float tmin = 0.0001, float tmax = 1e5) const
+{
 
-  if (!this->box.intersect(ray, tmin, tmax)) {
+  if (!this->box.intersect(ray, tmin, tmax))
+  {
     return false;
   }
 
@@ -41,38 +44,51 @@ inline bool BVHNode::intersect(const Ray &ray, HitRecord &hit_record,
 // sort the primitives
 // put half in each subtree
 inline BVHNode::BVHNode(ObjectList &object_list, uint i, uint j, float t0,
-                        float t1) {
+                        float t1)
+{
 
   int axis = gl::C_rand_int(0, 3);
   auto in_list = object_list;
   auto list_copy = in_list.getLists();
 
   auto compare = [&](std::shared_ptr<Hittable> p1, std::shared_ptr<Hittable> p2,
-                     int axis_index) {
-    return p1->getAABB(t0, t1).get_min()[axis_index] <
-           p2->getAABB(t0, t1).get_min()[axis_index];
+                     int axis_index)
+  {
+    auto aabb1 = p1->getAABB(t0, t1);
+    auto aabb2 = p2->getAABB(t0, t1);
+    // Use center point instead of min for better handling of thin AABBs
+    return (aabb1.get_min()[axis_index] + aabb1.get_max()[axis_index]) * 0.5f <
+           (aabb2.get_min()[axis_index] + aabb2.get_max()[axis_index]) * 0.5f;
   };
 
   auto comparator =
       std::bind(compare, std::placeholders::_1, std::placeholders::_2, axis);
 
   // single element
-  if (j - i == 1) {
+  if (j - i == 1)
+  {
     this->left = list_copy[i];
     this->right = list_copy[i];
     // 2 elements, compare the min
-  } else if (j - i == 2) {
+  }
+  else if (j - i == 2)
+  {
 
-    if (compare(list_copy[i], list_copy[i + 1], axis)) {
+    if (compare(list_copy[i], list_copy[i + 1], axis))
+    {
       this->left = list_copy[i];
       this->right = list_copy[i + 1];
-    } else {
+    }
+    else
+    {
       this->left = list_copy[i + 1];
       this->right = list_copy[i];
     }
 
     // 3 elements, sort and divide
-  } else {
+  }
+  else
+  {
     std::sort(list_copy.begin() + i, list_copy.begin() + j, comparator);
 
     uint mid = i + (j - i) / 2;
@@ -85,12 +101,15 @@ inline BVHNode::BVHNode(ObjectList &object_list, uint i, uint j, float t0,
   this->objtype = ObjType::BVH_OBJ;
 };
 
-inline void print_BVH(std::shared_ptr<BVHNode> root) {
+inline void print_BVH(std::shared_ptr<BVHNode> root)
+{
   std::vector<std::shared_ptr<Hittable>> curr, next;
   curr.push_back(root);
 
-  while (!curr.empty()) {
-    for (auto &node : curr) {
+  while (!curr.empty())
+  {
+    for (auto &node : curr)
+    {
       if (node->objtype == ObjType::BVH_OBJ)
         std::cout << "1 ";
       else
@@ -99,8 +118,10 @@ inline void print_BVH(std::shared_ptr<BVHNode> root) {
 
     std::cout << std::endl;
 
-    for (auto &node : curr) {
-      if (node->objtype == ObjType::BVH_OBJ) {
+    for (auto &node : curr)
+    {
+      if (node->objtype == ObjType::BVH_OBJ)
+      {
         next.push_back(std::dynamic_pointer_cast<BVHNode>(node)->left);
         next.push_back(std::dynamic_pointer_cast<BVHNode>(node)->right);
       }
@@ -111,8 +132,10 @@ inline void print_BVH(std::shared_ptr<BVHNode> root) {
 }
 
 inline void printBT(const std::string &prefix, std::shared_ptr<Hittable> node,
-                    bool isLeft) {
-  if (node != nullptr) {
+                    bool isLeft)
+{
+  if (node != nullptr)
+  {
     std::cout << prefix;
 
     std::cout << (isLeft ? "├──" : "└──");
@@ -120,7 +143,8 @@ inline void printBT(const std::string &prefix, std::shared_ptr<Hittable> node,
     // print the value of the node
     std::cout << "1" << std::endl;
 
-    if (node->objtype == ObjType::BVH_OBJ) {
+    if (node->objtype == ObjType::BVH_OBJ)
+    {
       bool is_dup = false;
       if (std::dynamic_pointer_cast<BVHNode>(node)->left ==
           std::dynamic_pointer_cast<BVHNode>(node)->right)
--- include/utils/constants.hpp
+++ include/utils/constants.hpp
@@ -2,6 +2,7 @@
 #include "./matrix.hpp"
 #include <numeric>
 
-namespace gl {
-static constexpr float epsilon = 1e-6;
+namespace gl
+{
+    static constexpr float epsilon = 1e-3;
 }
